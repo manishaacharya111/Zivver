b4367a4e003c41ae58000e84385ebbe6b467515e
manishaacharya111 <manisha.acharya30@gmail.com>
2024-06-12 21:42:14 +0200

    added lb

---
+++ ECS-FARGATE-terraform/loadbalancer.tf
@@ -0,0 +1,33 @@
+resource "aws_lb" "lb" {
+  name               = "lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+}
+
+resource "aws_lb_target_group" "lb_target_group" {
+  name     = "lb-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    port                = 80
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 20
+  }
+}
+resource "aws_lb_listener" "ecs_listener" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group.arn
+  }
+}
