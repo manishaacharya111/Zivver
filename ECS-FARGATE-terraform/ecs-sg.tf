d7db79361fc2d675cda31b4dde41a33fc89b7a12
manishaacharya111 <manisha.acharya30@gmail.com>
2024-06-12 21:30:07 +0200

    loadbalancer sg

--- ECS-FARGATE-terraform/ecs-sg.tf
+++ ECS-FARGATE-terraform/ecs-sg.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "sg" {
-  name   = "ecs-sg"
+resource "aws_security_group" "lb_sg" {
+  name   = "lb_sg"
   vpc_id = aws_vpc.main.id
 
   ingress {
@@ -8,7 +8,25 @@ resource "aws_security_group" "sg" {
     protocol    = "tcp"
     self        = "false"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "http"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+resource "aws_security_group" "ecs-sg" {
+  name   = "ecs-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = []
+    security_groups = [ aws_security_group.lb_sg ]
   }
 
   egress {
@@ -18,3 +36,4 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
