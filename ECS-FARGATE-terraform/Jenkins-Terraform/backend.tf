2c145c8f3d9f965b0e273c52358636f35fc238a7
manishaacharya111 <116179205+manishaacharya111@users.noreply.github.com>
2024-06-12 19:18:00 +0200

    Delete ECS-FARGATE-terraform/Jenkins-Terraform directory

--- ECS-FARGATE-terraform/Jenkins-Terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "zivver-terraform-s3-bucket" # Provide the actual S3 bucket name created in console
-    key    = "zivver/jenkins/terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "dynamodb-state-locking"
-  }
-}
