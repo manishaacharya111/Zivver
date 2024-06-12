64fdf72ee53d477b2162b821c7a7c9a93a3a0565
manishaacharya111 <manisha.acharya30@gmail.com>
2024-06-12 22:11:47 +0200

    update

--- ECS-FARGATE-terraform/main.tf
+++ ECS-FARGATE-terraform/main.tf
@@ -43,7 +43,7 @@ resource "aws_ecs_service" "service" {
 
   network_configuration {
     assign_public_ip = true
-    security_groups  = [aws_security_group.sg.id]
+    security_groups  = [aws_security_group.ecs-sg.id]
     subnets          = [aws_subnet.subnet.id]
   }
   lifecycle {
