e5868e7aa1f676c69e1c435ff43b8c407bb1d001
manishaacharya111 <manisha.acharya30@gmail.com>
2024-06-13 18:41:45 +0200

    updated version in provider terraform

--- ECS-FARGATE-terraform/loadbalancer.tf
+++ ECS-FARGATE-terraform/loadbalancer.tf
@@ -8,7 +8,7 @@ resource "aws_lb" "lb" {
 
 resource "aws_lb_target_group" "lb_target_group" {
   name        = "lb-target-group"
-  port        = 80
+  port        = 8080
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
@@ -25,7 +25,7 @@ resource "aws_lb_target_group" "lb_target_group" {
 }
 resource "aws_lb_listener" "ecs_listener" {
   load_balancer_arn = aws_lb.lb.arn
-  port              = "8080"
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
