e29d52d21005d29c51a64f4a43c4b5b96328ef0e
manishaacharya111 <manisha.acharya30@gmail.com>
2024-06-13 11:40:13 +0200

    updated

--- ECS-FARGATE-terraform/network.tf
+++ ECS-FARGATE-terraform/network.tf
@@ -17,6 +17,45 @@ resource "aws_subnet" "subnet_b" {
   availability_zone = "ap-south-1b"
 }
 
+resource "aws_security_group" "lb_sg" {
+  name   = "lb_sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    self        = "false"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "ecs-sg" {
+  name   = "ecs-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    cidr_blocks     = []
+    security_groups = [aws_security_group.lb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
